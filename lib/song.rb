f43acb336750df98cef82b4f1bcee7c54c802d18
Zitai Huang <mywpiedu@gmail.com>
2019-12-08 23:00:13 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,97 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count +=1 
+    
+  #  if   @@artists.all? {|art| art != artist }
+  #     @@artists << artist 
+  #  end 
+             
+    @@artists << artist 
+    
+    
+    @@genres << genre 
+    
+    
+    
+  end 
+  
+  
+  
+  def self.count 
+    @@count
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end 
+  
+  def self.genres 
+    @@genres.uniq 
+  end 
+  
+  def self.genre_count
+    genre_hash = {}
+    @@genres.uniq.each do |gen|
+      i = 0 
+      @@genres.each do |gen2|
+        if gen == gen2
+          i += 1 
+        end 
+      end 
+      genre_hash[gen]= i   
+    end 
+      
+  
+    return genre_hash
+  end 
+  
+  
+  
+  def self.artist_count
+    artist_hash = {}
+    @@artists.uniq.each do |gen|
+      i = 0 
+      @@artists.each do |gen2|
+        if gen == gen2
+          i += 1 
+        end 
+      end 
+      artist_hash[gen]= i   
+    end 
+      
+  
+    return artist_hash
+  
+  
+  
+  
+  
+  
+  
+  
+    
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
